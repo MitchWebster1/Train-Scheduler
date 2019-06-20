617ddb71d593dfa82ce76bb0e04551ecc36325c8
Mitch Webster <mitch83083@gmail.com>
2019-06-19 21:51:47 -0400

    created files / made table

---
+++ assets/javascript/index.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0WfWeJt_-_BpQzm0km0SspnTpGvvvo3o",
+  authDomain: "train-scheduler-4c2ff.firebaseapp.com",
+  databaseURL: "https://train-scheduler-4c2ff.firebaseio.com",
+  projectId: "train-scheduler-4c2ff",
+  storageBucket: "train-scheduler-4c2ff.appspot.com",
+  messagingSenderId: "591127548202",
+  appId: "1:591127548202:web:0c1d77456fbabf21",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
