0fecd250dfe1d40fda1e599b0a2edbb4c7623892
Mitch Webster <mitch83083@gmail.com>
2019-06-21 22:04:35 -0400

    database and displaying on page working

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -7,6 +7,50 @@ const firebaseConfig = {
   messagingSenderId: "591127548202",
   appId: "1:591127548202:web:0c1d77456fbabf21",
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 const database = firebase.database();
+const { format } = dateFns;
+const createEl = el => document.createElement(el);
+const select = id => document.getElementById(id);
+
+const setEl = (text) => {
+  const element = createEl("td");
+  element.innerHTML = text;
+  return element;
+};
+
+const createEntry = (event) => {
+  event.preventDefault();
+  const trainName = select("name").value;
+  const dest = select("destination").value;
+  const freq = select("frequency").value;
+  const trainTime = select("trainTime").value;
+  const newTrain = {
+    name: trainName,
+    destination: dest,
+    frequency: freq,
+    nextArrival: trainTime,
+    minutesAway: trainName,
+  };
+  database.ref().push(newTrain);
+  select("form").reset();
+};
+
+database.ref().on("child_added", (snapshot) => {
+  const row = createEl("tr");
+  const name = setEl(snapshot.val().name);
+  const dest = setEl(snapshot.val().destination);
+  const freq = setEl(snapshot.val().frequency);
+  const arrive = setEl(snapshot.val().nextArrival);
+  const minAway = setEl(snapshot.val().minutesAway);
+  console.log(name, dest, freq);
+  row.append(name, dest, freq, arrive, minAway);
+  select("tbody").append(row);
+});
+
+select("form").addEventListener("submit", createEntry);
+
+// console.log(trainName);
