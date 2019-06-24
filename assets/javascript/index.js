19fe054539a78a52ae73969acb38fcf5530d205f
Mitch Webster <mitch83083@gmail.com>
2019-06-24 19:22:30 -0400

    added html form validation / background image

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,56 +1,71 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD0WfWeJt_-_BpQzm0km0SspnTpGvvvo3o",
-  authDomain: "train-scheduler-4c2ff.firebaseapp.com",
-  databaseURL: "https://train-scheduler-4c2ff.firebaseio.com",
-  projectId: "train-scheduler-4c2ff",
-  storageBucket: "train-scheduler-4c2ff.appspot.com",
-  messagingSenderId: "591127548202",
-  appId: "1:591127548202:web:0c1d77456fbabf21",
-};
+  apiKey: 'AIzaSyD0WfWeJt_-_BpQzm0km0SspnTpGvvvo3o',
+  authDomain: 'train-scheduler-4c2ff.firebaseapp.com',
+  databaseURL: 'https://train-scheduler-4c2ff.firebaseio.com',
+  projectId: 'train-scheduler-4c2ff',
+  storageBucket: 'train-scheduler-4c2ff.appspot.com',
+  messagingSenderId: '591127548202',
+  appId: '1:591127548202:web:0c1d77456fbabf21'
+}
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const database = firebase.database();
-const { format } = dateFns;
-const createEl = el => document.createElement(el);
-const select = id => document.getElementById(id);
+const database = firebase.database()
+const createEl = el => document.createElement(el)
+const select = id => document.getElementById(id)
 
-const setEl = (text) => {
-  const element = createEl("td");
-  element.innerHTML = text;
-  return element;
-};
+// creates an element and sets the text to be displayed
+const setEl = text => {
+  const element = createEl('td')
+  element.innerHTML = text
+  return element
+}
 
-const createEntry = (event) => {
-  event.preventDefault();
-  const trainName = select("name").value;
-  const dest = select("destination").value;
-  const freq = select("frequency").value;
-  const trainTime = select("trainTime").value;
+// gathers information from the form fields and creates database entry
+const createEntry = event => {
+  event.preventDefault()
+  const trainName = select('name').value
+  const dest = select('destination').value
+  const freq = select('frequency').value
+  const trainTime = select('trainTime').value
   const newTrain = {
     name: trainName,
     destination: dest,
     frequency: freq,
-    nextArrival: trainTime,
-    minutesAway: trainName,
-  };
-  database.ref().push(newTrain);
-  select("form").reset();
-};
+    firstTrain: trainTime
+  }
+  database.ref().push(newTrain)
+  select('form').reset()
+}
 
-database.ref().on("child_added", (snapshot) => {
-  const row = createEl("tr");
-  const name = setEl(snapshot.val().name);
-  const dest = setEl(snapshot.val().destination);
-  const freq = setEl(snapshot.val().frequency);
-  const arrive = setEl(snapshot.val().nextArrival);
-  const minAway = setEl(snapshot.val().minutesAway);
-  console.log(name, dest, freq);
-  row.append(name, dest, freq, arrive, minAway);
-  select("tbody").append(row);
-});
+// calculates the minutes until the next train
+const minutesTillNextTrain = snapshot => {
+  const freq = snapshot.val().frequency
+  const firstTime = moment(snapshot.val().firstTrain, 'hh:mm').subtract(
+    1,
+    'years'
+  )
+  const difference = moment().diff(moment(firstTime), 'minutes')
+  const remainder = difference % freq
+  const timeTill = freq - remainder
+  return timeTill
+}
 
-select("form").addEventListener("submit", createEntry);
+// calculates the arrival time of the next train
+const arrival = minAway => moment().add(minAway, 'minutes')
 
-// console.log(trainName);
+// gathers the table data then appends the row
+database.ref().on('child_added', snapshot => {
+  const row = createEl('tr')
+  const name = setEl(snapshot.val().name)
+  const dest = setEl(snapshot.val().destination)
+  const freq = setEl(snapshot.val().frequency)
+  const timeTill = minutesTillNextTrain(snapshot)
+  const arrivalTIme = setEl(arrival(timeTill).format('hh:mm A'))
+  const minAway = setEl(timeTill)
+  row.append(name, dest, freq, arrivalTIme, minAway)
+  select('tbody').append(row)
+})
+
+select('form').addEventListener('submit', createEntry)
